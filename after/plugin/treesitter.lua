47f14c646a2589a7c5b408edb3328343ebee7ef5
sulthonuladib <adibcyber4rt@gmail.com>
2023-03-05 17:17:45 +0700

    feat(after/plugin): add some plugin with packer and keymapping

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "c", "lua", "help", "rust" },
+  ensure_installed = { "javascript", "typescript", "c", "lua", "help", "rust", "query", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
